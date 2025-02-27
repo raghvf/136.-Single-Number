0c2c150a36ca87edbd523e10221164222e97bacc
raghvf <bidhyarthimajumder45@gmail.com>
2025-02-28 01:35:12 +0530

    with fuction

--- vecinp.cpp
+++ vecinp.cpp
@@ -2,22 +2,27 @@
 #include <vector>
 using namespace std;
 
-int main(){
+int vecJ(){
     int n;
-    cout<<"Enter the size of vector : ";
+    cout<<"Enter the no. of elements : ";
     cin>>n;
     vector<int> vec(n);
-    cout<<"Enter the elements : ";
-    for(int i=0; i<vec.size(); i++){
+
+    cout<<" Enter the elements : ";
+    for(int i=0; i<n; i++){
         cin>>vec[i];
     }
 
-
-    cout<<" Elements are : "<<endl;
-    for(int i=0; i<vec.size(); i++){
-        cout<<vec[i]<<endl;
+    cout<<"Elements are : "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<vec[i]<<" "<<endl;
     }
 
+    
+}
+
+int main(){
+    vecJ();
 
     return 0;
 }
